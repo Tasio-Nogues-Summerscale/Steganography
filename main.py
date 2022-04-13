6f8fa6a7170e90643ab3fffc1977ed190857eb2a
Tasio <tasionogues12345@gmail.com>
2022-04-13 08:15:41 +0100

    removed unnecessary import

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 from PIL import Image
-import pathlib
 import sys
 import argparse
 import numpy as np
-from unidecode import unidecode
 
-directory = pathlib.Path(__file__).parent
 
 
 def encode(image: Image, text: str) -> Image:
+    text += chr(0)
     binary_text = [int(val) for val in "".join(format(ord(char), "08b") for char in text)]
-    binary_text += [0,0,0,0,0,0,0,0] #add stopping symbol
-    #print(binary_text)
+    
     bin_len = len(binary_text)
-    print(bin_len)
-    print(image.height*image.width*3)
     if bin_len > (image.width * image.height) * 3:
         raise ValueError("Text too large for this image")
-    image_array = np.array(img)
-    print(image_array.shape)
+    image_array = np.array(image)
     i = 0
     for x in range(image.width):
         for y in range(image.height):
@@ -26,11 +20,9 @@ def encode(image: Image, text: str) -> Image:
                 binary_val = (image_array[y][x][j] & ~1) | binary_text[i]
                 image_array[y][x][j] = binary_val
                 i += 1
-                if i >= bin_len:
+                if i == bin_len:
                     return Image.fromarray(image_array, "RGBA")
 
-def decode_binary_string(s):
-    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
 
 def decode(image: Image):
     binary_string = ""
@@ -41,10 +33,10 @@ def decode(image: Image):
             for j in range(3):
                 binary_string += str(image_array[y][x][j] & 1)
                 
-                if i % 8 == 0:
+                if (i+1) % 8 == 0:
                     if binary_string[-8:] == "00000000":
                         binary_int = int(binary_string[:-8], 2) #convert to integer
-                        return decode_binary_string(binary_string[:-8])
+                        return binary_int.to_bytes((binary_int.bit_length() + 7) //8, "big").decode("iso-8859-1")
                 i += 1
 
 
@@ -62,19 +54,15 @@ if __name__ == "__main__":
     if not(any([(args.encode or args.decode), args.imagefile])):
         sys.exit()
 
-    img = Image.open(args.imagefile.name)
-    img = img.convert("RGBA")
+    img = Image.open(args.imagefile.name).convert("RGBA")
 
     if args.encode:
         text = args.text
         if args.textfile:
             with open(args.textfile.name, "r") as f:
-                text = f.read()
+                text = f.read().rstrip()
 
-        text = unidecode(text)
-        print(text)
-        
-
+        text = text.encode("iso-8859-1","ignore").decode("iso-8859-1")
         new_img = encode(img, text)
         if args.outputfile:
             new_img.save(args.outputfile)
